b1ccfb1a5d9153039751d1c4810806b38b4dbf5d
Nikhil Gohil <nikhil.n.gohil@gmail.com>
2020-09-30 14:53:53 -0700

    Adding Insertion Sort

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -1,21 +1,35 @@
 from Algorithms.SelectionSort import SelectionSort
 from Algorithms.BubbleSort import BubbleSort
+from Algorithms.InsertionSort import InsertionSort
 import numpy as np
 import time
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 def testSelectionSort(myarray):
     start_time = time.time()
-    SelectionSort(myarray)
+    sorted = SelectionSort(myarray)
+    #pp.pprint(sorted)
     print("SelectionSort took {:.5f} to complete!".format(time.time()-start_time))
 
 def testBubbleSort(myarray):
     start_time = time.time()
-    BubbleSort(myarray)
+    sorted = BubbleSort(myarray)
+    pp.pprint(sorted)
     print("BubbleSort took {:.5f} to complete!".format(time.time()-start_time))
 
+def testInsertionSort(myarray):
+    start_time = time.time()
+    sorted = InsertionSort(myarray)
+    pp.pprint(sorted)
+    print("InsertionSort took {:.5f} to complete!".format(time.time()-start_time))
+
 def main():
-    array = np.random.randint(-100000, 100000, 1000)
+    array = np.random.randint(0, 100000, 20)
     testSelectionSort(array)
     testBubbleSort(array)
+    testInsertionSort(array)
+
 
 if __name__ == "__main__":
     main()
