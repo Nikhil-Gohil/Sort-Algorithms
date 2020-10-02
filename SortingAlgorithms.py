2320bf59042e9daf5ee12559351d3a39a9d2911c
Nikhil Gohil <nikhil.n.gohil@gmail.com>
2020-10-01 18:15:53 -0700

    Testing out Black formatter

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -5,31 +5,39 @@ from Algorithms.InsertionSort import InsertionSort
 import numpy as np
 import time
 import pprint
+
 pp = pprint.PrettyPrinter(indent=4)
+
 
 def testSelectionSort(myarray):
     start_time = time.time()
     SelectionSort(myarray)
-    #pp.pprint(sorted)
-    print("SelectionSort took {:.5f} to complete!".format(time.time()-start_time))
+    # pp.pprint(sorted)
+    print("SelectionSort took {:.5f} to complete!".format(time.time() - start_time))
+
 
 def testBubbleSort(myarray):
     start_time = time.time()
     BubbleSort(myarray)
-    #pp.pprint(sorted)
-    print("BubbleSort took {:.5f} to complete!".format(time.time()-start_time))
+    # pp.pprint(sorted)
+    print("BubbleSort took {:.5f} to complete!".format(time.time() - start_time))
+
 
 def testBubbleSortRecursive(myarray):
     start_time = time.time()
     sorted = BubbleSortRecursive(myarray)
     pp.pprint(sorted)
-    print("Recursive BubbleSort took {:.5f} to complete!".format(time.time()-start_time))
+    print(
+        "Recursive BubbleSort took {:.5f} to complete!".format(time.time() - start_time)
+    )
+
 
 def testInsertionSort(myarray):
     start_time = time.time()
     InsertionSort(myarray)
-    #pp.pprint(sorted)
-    print("InsertionSort took {:.5f} to complete!".format(time.time()-start_time))
+    # pp.pprint(sorted)
+    print("InsertionSort took {:.5f} to complete!".format(time.time() - start_time))
+
 
 def main():
     array = np.random.randint(0, 100000, 2500)
